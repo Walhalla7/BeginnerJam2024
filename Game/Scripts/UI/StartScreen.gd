d9ea7f38bd795ae775c409da9bc8d5947bf3da81
Walhalla7 <bartoszrdzanekwp@gmail.com>
2024-08-10 17:23:23 -0400

    Added UI

---
+++ Game/Scripts/UI/StartScreen.gd
@@ -0,0 +1,24 @@
+extends Control
+
+#Preloading Scene
+var main# = preload("res://world.tscn")
+var intro #= preload("res://intro.tscn")
+
+#Show at the beginning
+func _ready():
+	SignalBus.intro_done.connect(_on_intro_done)
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	show()
+
+#starting Game
+func _on_start_button_pressed():
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	hide()
+	#initialize intro scene
+	var intro_instance = intro.instantiate()
+	get_parent().add_child(intro_instance)
+
+	
+func _on_intro_done():
+	var newLevel_instance = main.instantiate()
+	get_parent().add_child(newLevel_instance)
