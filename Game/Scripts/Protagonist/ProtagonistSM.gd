ee320f691d964c3eda267f4b063ef811bca3cc3c
Walhalla7 <bartoszrdzanekwp@gmail.com>
2024-08-11 16:10:56 -0400

    Smoother Realm switch
    - now you can press q and e to go forward or backwards in realm list
    - now you assign which realm something belongs in the individual scene

--- Game/Scripts/Protagonist/ProtagonistSM.gd
+++ Game/Scripts/Protagonist/ProtagonistSM.gd
@@ -1,9 +1,37 @@
 extends StateMachine
 
+#------------------------------- REALM MANAGMENT -------------------------------
+#var Q_Realm = "Alt1"
+#var E_Realm = "Alt2"
+#var Curr_Realm = "Default"
+var currRealm = 2
+
+
 #function to handle jump input
 func _input(event):
 	if event.is_action_released("ChangeWorld"):
-			SignalBus.emit_signal("changeWorld")
+		currRealm -= 1
+		if currRealm < 1:
+			currRealm = 3
+		match currRealm:
+			1: 
+				SignalBus.emit_signal("changeWorld", "Alt1")
+			2:
+				SignalBus.emit_signal("changeWorld", "Default")
+			3:
+				SignalBus.emit_signal("changeWorld", "Alt3")
+		
+	if event.is_action_released("ChangeWorld1"):
+		currRealm += 1
+		if currRealm > 3:
+			currRealm = 1
+		match currRealm:
+			1: 
+				SignalBus.emit_signal("changeWorld", "Alt1")
+			2:
+				SignalBus.emit_signal("changeWorld", "Default")
+			3:
+				SignalBus.emit_signal("changeWorld", "Alt3")
 
 func _ready():
 	#we add all the posible states to the list 
